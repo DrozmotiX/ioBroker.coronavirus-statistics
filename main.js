0058783886cdd9ce6763f5e7803519e028c1a361
Dutchman <rdrozda86@gmail.com>
2020-03-22 13:34:55 +0100

    move country translator to lib

--- main.js
+++ main.js
@@ -14,28 +14,7 @@ const { wait } = require('./lib/tools');
 const countryJs = require('country-list-js');
 
 // Translator if country names are not iso conform
-const countryTranslator = {
-	// https://github.com/i-rocky/country-list-js/blob/master/data/names.json
-	'Vatican_City': 'Vatican',
-	'USA': 'United States',
-	'UK': 'United Kingdom',
-	'UAE': 'United Arab Emirates',
-	'US_Virgin_Islands': 'U.S. Virgin Islands',
-	'St_Vincent_Grenadines': 'Saint Vincent and the Grenadines',
-	'St_Barth': 'Saint Barthelemy',
-	'S_Korea': 'South Korea',
-	'Palestine': 'Palestinian Territory',
-	'North_Macedonia': 'Macedonia',
-	'Faeroe_Islands': 'Faroe Islands',
-	'Eswatini': 'Swaziland',
-	'Czechia': 'Czech Republic',
-	'Congo': 'Republic of the Congo',
-	'CAR': 'Central African Republic',
-	'DRC': 'Democratic Republic of the Congo',
-	'Channel_Islands': 'France',                             // gehört zu Europa, deshalb Frankreich einfach vergeben
-	'Cabo_Verde': 'Cape Verde',
-	'Timor-Leste': 'East Timor'
-}
+const countryTranslator = require('./lib/countryTranslator');
 
 class Covid19 extends utils.Adapter {
 	/**
